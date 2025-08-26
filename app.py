55477086e0c0cd615ca0b922f539968332958b55
Santiago Guerra <santiago.guerra76@yahoo.com>
2025-08-25 19:25:54 -0500

    agregamos requiremts y add

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
